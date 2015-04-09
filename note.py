6e7bb33ab372104a1d68300faa564cd2ade3a8b8
Vitor <jvnsoares@gmail.com>
2015-04-08 21:38:56 -0300

    note.py completely changed, now use the sqlite to store the notes in a persistent way.

--- note.py
+++ note.py
@@ -1,57 +1,86 @@
-import json
+import sqlite3 as lite
 import exceptions
 import sys
 
 
+##clss notes, within this classe are functions to read, list, write and delete notes using a sqlite database
 class Notes:
-    def __init__(self, note_dic=None,
-                 bkp_file=None):
-        self.note_dic = note_dic or dict()
-        self.bkp_file = bkp_file or '.notes'
+
+
+    def __init__(self):
+
         self.load()
 
+
+    ##load function, it is possible to choose between the default database or create/use a new one
     def load(self):
-        bkp = open(self.bkp_file, 'r')
-        self.note_dic = json.loads(bkp.read())
 
+        sys.stdout.write("Database name? ")
+        self.database = sys.stdin.readline().strip()
+
+        if self.database == "":
+            sys.stdout.write("Default database loaded \n")
+            self.database="note"
+
+        with lite.connect('%s.db' % (self.database )) as con:
+            cur=con.cursor()
+            cur.execute("CREATE TABLE IF NOT EXISTS notes(id  INTEGER PRIMARY KEY AUTOINCREMENT, title TEXT NOT NULL UNIQUE, content TEXT)")
+
+    ##read function, to read a note is necessary give the note title
     def read(self):
-        while True:
-            sys.stdout.write("Which? ")
-            name = sys.stdin.readline()\
-                .strip()
-            try:
-                print(self.note_dic[name])
-                break
-            except exceptions.KeyError:
-                pass
+        sys.stdout.write("Title? ")
+        title = sys.stdin.readline().strip()
 
+        with lite.connect('%s.db' % (self.database )) as con:
+            cur = con.cursor()
+            cur.execute("SELECT notes.content FROM notes WHERE notes.title = '%s' " % (title) )
+            result = cur.fetchall()
+
+            if not result:
+                sys.stdout.write( "Note not found\n")
+
+            for row in result:
+                sys.stdout.write( row[0] + '\n' )
+
+
+    ##write function, to write a new note is necessary create a title and the content of the function.
+    ##The id it is created automatically
     def write(self):
-        sys.stdout.write("Name? ")
-        name = sys.stdin.readline().strip()
-        sys.stdout.write("Body? ")
-        body = sys.stdin.readline().strip()
-        self.note_dic[name] = body
 
-    def delete(self):
-        while True:
-            sys.stdout.write("Which? ")
-            name = sys.stdin.readline()\
-                .strip()
-            try:
-                del self.note_dic[name]
-                break
-            except exceptions.KeyError:
-                pass
+        sys.stdout.write("Title: ")
+        title = sys.stdin.readline().strip()
 
+        sys.stdout.write("Content: ")
+        content = sys.stdin.readline().strip()
+
+        with lite.connect('note.db') as con:
+            result=con.execute("INSERT INTO notes(title,content) VALUES( '%s' , '%s' )" % (title,content))
+
+
+    ##delet function, to delet a note is is necessary pass the note title
+    # todo: include a way to delet a note using the note id
+    def delet(self):
+
+        sys.stdout.write("Title: ")
+        title = sys.stdin.readline().strip()
+
+
+        with lite.connect('%s.db' % (self.database )) as con:
+            result=con.execute("DELETE FROM notes WHERE notes.title = '%s' " % (title))
+            for row in result:
+                sys.stdout.write( row[0] + '\n' )
+
+
+    ##list the notes in the database
     def list(self):
-        for name in self.note_dic:
-            print(name)
+       with lite.connect('%s.db' % (self.database )) as con:
+            result=con.execute(" SELECT notes.id, notes.title FROM notes ")
+            for row in result:
+                 sys.stdout.write( str( row[0] ) + '-' + row[1] + '\n')
+
 
     def persist(self):
-        bkp_file = open(self.bkp_file, 'w')
-        bkp_file.write(
-            json.dumps(self.note_dic))
-        bkp_file.close()
+        pass
 
 
 #read, list, write, delete, save
@@ -68,9 +97,9 @@ def run():
         elif action == 'w':
             notes.write()
         elif action == 'd':
-            notes.delete()
-        elif action == 's':
-            notes.persist()
+            notes.delet()
+        #elif action == 's':
+        #    notes.persist()
         elif action == 'q':
             break
 
